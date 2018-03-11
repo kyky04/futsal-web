a53800c6fe798da54e7702af816856cbcbf82cff
Ucup 2 <rizki@pragmainf.co.id>
2018-03-12 03:48:17 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,12 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::group(['middleware' => 'auth:api'], function () {
+	Route::get('/user', function (Request $request) {
+    	return $request->user();
+    });
+  });
+
 
 Route::resource('lapangan', 'LapanganAPIController');
 
